cc8a58031423db3f457da21587d3ebfc8e9a1fde
Татаренко Александр Петрович <aleksandr.tatarenko@alfabank.kiev.ua>
2020-01-29 18:51:39 +0200

    add 5 work

--- PracticalWork4/src/work46/Task46.java
+++ PracticalWork4/src/work46/Task46.java
@@ -7,6 +7,10 @@ public class Task46 {
         Scanner scan = new Scanner(System.in);
         System.out.println("enter number");
         int input = scan.nextInt();
+        input = input < 0 ? -input : input;
+//        if (input < 0){
+//            input = -input;
+//        }
         int res = 0;
         while (input > 0){
             res += input % 10 * input % 10;
